14f46ebf87c95ad3dac277e4a32687a3cec60e74
Mariam Akhtar <mariam7532@gmail.com>
2020-08-16 04:42:36 +0800

    prettier

--- src/App.js
+++ src/App.js
@@ -1,106 +1,122 @@
-import React, { Component } from 'react';
-import './App.css';
-import Dropdown from './components/dropdown';
-
+import React, { Component } from "react";
+import "./App.css";
+import Dropdown from "./components/dropdown";
 
 class App extends Component {
-  constructor(){
-    super()
-    this.toggleSelected = this.toggleSelected.bind(this)
-    this.searchHandler = this.searchHandler.bind(this)
-    this.addLocation = this.addLocation.bind(this)
+  constructor() {
+    super();
+    this.toggleSelected = this.toggleSelected.bind(this);
+    this.searchHandler = this.searchHandler.bind(this);
+    this.addLocationHandler = this.addLocationHandler.bind(this);
+    this.showMoreHandler = this.showMoreHandler.bind(this);
     this.state = {
-      location:[],
-      filtered:[],
-      addBtn:false,
-      selectedItem : ''
-    }
+      location: [],
+      filtered: [],
+      addBtn: false,
+      showItems: 2,
+      selectedItem: "",
+    };
   }
-  
-  
+
   componentDidMount() {
-    fetch('https://run.mocky.io/v3/7b2ad53f-98e4-442d-b274-c406ab78525d')
-    .then(res => res.json())
-    .then((data) => {
-      console.log("data", data);
-      this.setState({ location: data , filtered:data})
-      
-    })
-    .catch(console.log)
+    fetch("https://run.mocky.io/v3/7b2ad53f-98e4-442d-b274-c406ab78525d")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data);
+        this.setState({ location: data, filtered: data });
+      })
+      .catch(console.log);
   }
-  toggleSelected(id, key){
-    
+  toggleSelected(id, key) {
     let temp = this.state[key];
-    temp[id].selected = !temp[id].selected
+    temp[id].selected = !temp[id].selected;
     this.setState({
-      [key]: temp
-    })
-  
+      [key]: temp,
+    });
   }
-  addLocation(event){
-    
+  addLocationHandler(event) {
     let temp = [...this.state.location];
-    console.log("vale new",this.state.selectedItem);
-    const loc =  {id: temp.length, title: this.state.selectedItem, selected: false, key: "location"};
+    console.log("vale new", this.state.selectedItem);
+    const loc = {
+      id: temp.length,
+      title: this.state.selectedItem,
+      selected: false,
+      key: "location",
+    };
     temp.push(loc);
     this.setState({
-
       location: temp,
-      filtered:temp,
-    }) 
+      filtered: temp,
+      showItems: temp.length,
+      selectedItem: "",
+    });
+    this.setState((prevState) => ({
+      addBtn: !prevState.addBtn,
+    }));
+  }
 
-    }
-  
-
+  showMoreHandler(showMoreItems) {
+    console.log("show more", showMoreItems);
+    console.log("show More ", this.state.filtered);
+    this.setState({
+      showItems:
+        this.state.showItems >= this.state.location.length
+          ? this.state.showItems
+          : this.state.location.length,
+    });
+  }
 
   searchHandler(e) {
-    let currentList =[...this.state.location];
+    let currentList = [...this.state.location];
     let newList = [];
     let searchString = e.target.value;
     // If the search bar isn't empty
-if (e.target.value !== "") {
-        // Assign the original list to currentList
-      console.log("string", e.target.value)
+    if (e.target.value !== "") {
+      // Assign the original list to currentList
+      console.log("string", e.target.value);
 
-        // Use .filter() to determine which items should be displayed
-        // based on the search terms
-  newList = currentList.filter(item => item.title.toLowerCase().includes(searchString.toLowerCase()));
-  this.setState({
-    filtered: newList
-  })
-  const isAdmin = true // check add priviledge
-  
-  if(newList.length<1 && isAdmin) {
-    this.setState({
-      addBtn: true,
-      selectedItem : e.target.value
-    })
+      // Use .filter() to determine which items should be displayed
+      // based on the search terms
+      newList = currentList.filter((item) =>
+        item.title.toLowerCase().includes(searchString.toLowerCase())
+      );
+      this.setState({
+        filtered: newList,
+        selectedItem: e.target.value,
+      });
+      const isAdmin = true; // check add priviledge
+      //Click show all before add button
+      if (newList.length < 1 && isAdmin) {
+        this.setState({
+          addBtn: true,
+          selectedItem: e.target.value,
+        });
+      }
+    } else {
+      // If the search bar is empty, set newList to original task list
+      this.setState({
+        filtered: currentList,
+        addBtn: false,
+        selectedItem: "",
+      });
+    }
   }
-} else {
-        // If the search bar is empty, set newList to original task list
-    this.setState({
-      filtered: currentList,
-    })
-}
- 
 
-}
-
-
-  render () {
+  render() {
     return (
       <div>
         <Dropdown
-        titleHelper="Location"
-  title="Select location"
-  selectedItem = {this.state.selected}
-  list={this.state.filtered}
-  toggleItem={this.toggleSelected}
-  searchItem ={this.searchHandler}
-  addItem= {this.addLocation}
-  addBtn = {this.state.addBtn}
-/>
-       
+          titleHelper="Location"
+          title="Select a location"
+          noOfRecord={this.state.showItems}
+          selectedItem={this.state.selectedItem}
+          list={this.state.filtered}
+          toggleItem={this.toggleSelected}
+          searchItem={this.searchHandler}
+          addItem={this.addLocationHandler}
+          showMoreItems={this.showMoreHandler}
+          addBtn={this.state.addBtn}
+        />
       </div>
     );
   }
--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,89 +1,116 @@
- 
-import React, { Component } from 'react';
-import FontAwesome from 'react-fontawesome';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes, faSearch, faCartArrowDown, faArrowDown, faCaretDown } from '@fortawesome/free-solid-svg-icons'
-import './dropdown.css';
+import React, { Component } from "react";
+import FontAwesome from "react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTimes,
+  faSearch,
+  faCartArrowDown,
+  faArrowDown,
+  faCaretDown,
+} from "@fortawesome/free-solid-svg-icons";
+import "./dropdown.css";
 
 export default class Dropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputVal: "",
+      listOpen: false,
+      headerTitle: this.props.title,
+      list: this.props.list,
+      addBtn: this.props.addBtn,
+      noOfRecords: this.props.noOfRecord,
+    };
+  }
+  static getDerivedStateFromProps(nextProps) {
+    const count = nextProps.list.filter(function (a) {
+      return a.selected;
+    }).length;
+    // const inputV = nextProps.selectedItem
+    // console.log('count',inputV)
 
-	constructor(props){
-        super(props)
-        this.state = {
-          inputVal:'',
-          listOpen: false,
-          headerTitle: this.props.title,
-          list:this.props.list,
-          addBtn: this.props.addBtn,
-          noOfRecords : this.props.noOfRecord
-        }
-      }
-      static getDerivedStateFromProps(nextProps){
-        const count = nextProps.list.filter(function(a) { return a.selected; }).length;
-        // const inputV = nextProps.selectedItem 
-       // console.log('count',inputV)
-    
-    if(count === 0){
-          return {headerTitle: nextProps.title}
-        }
-        else if(count === 1){
-          return {headerTitle: `${count} ${nextProps.titleHelper}`}
-        }
-        // else if(count > 1){
-        //   return {headerTitle: `${count} ${nextProps.titleHelper}s`}
-        // }
-      }
+    if (count === 0) {
+      return { headerTitle: nextProps.title };
+    } else if (count === 1) {
+      return { headerTitle: `${count} ${nextProps.titleHelper}` };
+    }
+    // else if(count > 1){
+    //   return {headerTitle: `${count} ${nextProps.titleHelper}s`}
+    // }
+  }
 
-    handleClickOutside(){
-        this.setState({
-          listOpen: false
-        })
-      }
-      toggleList(){
-        this.setState(prevState => ({
-          listOpen: !prevState.listOpen,
-          list:prevState.list
-        }))
-      }
+  handleClickOutside() {
+    this.setState({
+      listOpen: false,
+    });
+  }
+  toggleList() {
+    this.setState((prevState) => ({
+      listOpen: !prevState.listOpen,
+      list: prevState.list,
+    }));
+  }
 
-      
-    
-      render(){
-        const btnStyle = {
-          float : 'none'
-        };
-        
-        const{list} = this.props
-        const{inputVal,listOpen, headerTitle,noOfRecords } = this.state
-        const showMore = ((this.props.noOfRecord != list.length)&&(list.length>0)) ? true : false
-      
-        return(
-          <div className="container DropdownBox">
-           
-      <div  onClick={() => this.toggleList()}>
-              <div className="dd-header-title">{headerTitle} <FontAwesomeIcon icon={faCaretDown} /></div>
-              {listOpen
-               
-              }
+  render() {
+    const btnStyle = {
+      float: "none",
+    };
+
+    const { list } = this.props;
+    const { inputVal, listOpen, headerTitle, noOfRecords } = this.state;
+    const showMore =
+      this.props.noOfRecord != list.length && list.length > 0 ? true : false;
+
+    return (
+      <div className="container DropdownBox">
+        <div onClick={() => this.toggleList()}>
+          <div className="dd-header-title">
+            {headerTitle} <FontAwesomeIcon icon={faCaretDown} />
           </div>
-            {listOpen &&    <div className=""> <FontAwesomeIcon icon={faSearch} /><input type="text" className="input" value={this.props.selectedItem} onChange={(event)=>this.props.searchItem(event)} placeholder="Search..." />
-            </div>}
-              {this.props.addBtn &&
-            
-     <div>     
-          <p>{this.props.selectedItem}" not found</p>
-          <button style={btnStyle}  onClick={(e) => this.props.addItem(e)}>Add & Select</button></div> }
-      {listOpen && <ul className="dd-list">
-             {list.slice(0, this.props.noOfRecord).map((item, index) => (
-              <li className="dd-list-item" key={item.title} onClick={() => this.props.toggleItem(item.id, item.key)}>{item.title} {item.selected && <FontAwesome name="check"/>}</li>
-              ))}   
-             { showMore && <p onClick={() => this.props.showMoreItems(this.props.noOfRecord)}>{this.props.noOfRecord} more...</p> }
-            </ul>
-            
-            }
-             
+          {listOpen}
+        </div>
+        {listOpen && (
+          <div className="">
+            {" "}
+            <FontAwesomeIcon icon={faSearch} />
+            <input
+              type="text"
+              className="input"
+              value={this.props.selectedItem}
+              onChange={(event) => this.props.searchItem(event)}
+              placeholder="Search..."
+            />
           </div>
-          
-        )
-      }
+        )}
+        {this.props.addBtn && (
+          <div>
+            <p>{this.props.selectedItem}" not found</p>
+            <button style={btnStyle} onClick={(e) => this.props.addItem(e)}>
+              Add & Select
+            </button>
+          </div>
+        )}
+        {listOpen && (
+          <ul className="dd-list">
+            {list.slice(0, this.props.noOfRecord).map((item, index) => (
+              <li
+                className="dd-list-item"
+                key={item.title}
+                onClick={() => this.props.toggleItem(item.id, item.key)}
+              >
+                {item.title} {item.selected && <FontAwesome name="check" />}
+              </li>
+            ))}
+            {showMore && (
+              <p
+                onClick={() => this.props.showMoreItems(this.props.noOfRecord)}
+              >
+                {this.props.noOfRecord} more...
+              </p>
+            )}
+          </ul>
+        )}
+      </div>
+    );
+  }
 }
