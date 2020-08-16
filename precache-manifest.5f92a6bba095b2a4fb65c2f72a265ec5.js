63d2f55730dfe596a37d5b87101fcda1e6a7e144
Mariam Akhtar <mariam7532@gmail.com>
2020-08-16 13:23:20 +0800

    Updates

---
+++ precache-manifest.5f92a6bba095b2a4fb65c2f72a265ec5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c221b9cd742a2894577cd270e0168d",
+    "url": "/country-search-test/index.html"
+  },
+  {
+    "revision": "5c87dd863df0a538e2da",
+    "url": "/country-search-test/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4f2a09f4592d24890c94",
+    "url": "/country-search-test/static/css/main.c9f09362.chunk.css"
+  },
+  {
+    "revision": "5c87dd863df0a538e2da",
+    "url": "/country-search-test/static/js/2.2e0eb141.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country-search-test/static/js/2.2e0eb141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2a09f4592d24890c94",
+    "url": "/country-search-test/static/js/main.f203176e.chunk.js"
+  },
+  {
+    "revision": "ccbb38a98bb4c0cd17ce",
+    "url": "/country-search-test/static/js/runtime-main.341abd2a.js"
+  }
+]);
